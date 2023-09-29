4b6af71af1eea0cd4a5890ea7a9b8b35b4bebffe
Etyhae <84091920+Etyhae@users.noreply.github.com>
2023-09-29 16:08:07 +0300

    introduction of changes (#2)
    * chore(App.js) redesign some elements
    
    * chore(Chart.js) style chart
    
    * docs edit README
    
    ---------
    
    Co-authored-by: ghoul <dead@xdd.ru>

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import { BiMenu } from "react-icons/bi";
 
 import React, { useState} from "react";
 
+import Chart from "./components/Chart";
+import { FastAverageColor } from "fast-average-color";
+
 const song = {
 	playlistID: 29219219129,
 	playlistName: "DRESSCODE",
@@ -46,6 +49,12 @@ const song3 = {
 const songs = [song, song2, song3];
 
 
+const getBGColor = async (image) => {
+	const fac = new FastAverageColor;
+	const color = await fac.getColorAsync(image);
+	return color.hex;
+}
+
 function App() {
 	const [isHidden, setIsHidden] = useState(true);
 
@@ -54,6 +63,66 @@ function App() {
 	};
 
 	return (
+
+		<div>
+			<div className="min-h-screen">
+				<div className="bg-stone-800 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${song.coverURL})` }}>
+					<aside className={`${(isHidden? "translate-x-[100%]" : "translate-x-0")} lg:translate-x-0 transition delay-150 duration-300 ease-linear z-40 max-w-[100%] lg:max-w-[30%] lg:right-0 min-h-screen absolute lg:relative flex-col lg:float-right items-center text-left justify-start sm:min-w-[20%] bg-slate-700/30 h-[50%] pt-4 space-y-6 backdrop-blur-lg lg:flex`}>
+						{songs.map((song) => (
+							<Playlist song={song} />
+						))}
+					</aside>
+					<header className="h-screen flex flex-col items-center justify-center content-center text-base text-white backdrop-blur-lg backdrop-brightness-50 relative">
+						<div className="absolute inset-x-0 top-0 content-start mt-2">
+							<p className="text-sm text-center text-slate-400">Сейчас играет</p>
+							<p className="text-base text-center">{song.playlistName}</p>
+						</div>
+						<div className="flex flex-col items-center justify-center text-white font-mono mt-4">
+							<img
+								src={song.coverURL}
+								className="song-cover h-[40vh] w-[40vh] object-cover pointer-events-none mb-4 rounded-full"
+								alt="logo"
+							/>
+							
+							<p className="text-2xl font-semibold underline decoration-solid underline-offset-8 leading-loose">
+								{song.label}
+							</p>
+							<p className="text-xl text-slate-400">{song.author}</p>
+						</div>
+					</header>
+					<section className="z-50 fixed flex w-full bg-slate-400/20 p-2 bottom-0">
+						<div className="flex justify-start h-full w-full">
+							<Player url={song.songURL} size="6" />
+						</div>
+						<div className="lg:hidden flex items-center justify-end h-full w-full">
+							<button className="hover:opacity-80" onClick={handleClick}>
+								<IconContext.Provider value={{ size: "6vh", color: "#27AE60" }}>
+									<BiMenu />
+								</IconContext.Provider>
+							</button>
+						</div>
+					</section>
+				</div>
+			</div>
+			<div className="min-h-screen">
+				<div className={`flex w-screen bg-[${getBGColor(song.coverURL)}]`}>
+					<div className="w-1/3 py-12 pl-12">
+						{songs.map((song) => (
+							<Chart song={song} />
+						))}
+					</div>
+					<div className="w-1/3 py-12">
+						{songs.map((song) => (
+							<Chart song={song} />
+						))}
+					</div>
+					<div className="w-1/3 py-12 pr-12">
+						{songs.map((song) => (
+							<Chart song={song} />
+						))}
+					</div>
+
+				</div>
 		<div className="min-h-screen">
 			<div className="text-center bg-stone-800 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${song.coverURL})` }}>
 				<aside className={`${(isHidden? "translate-x-[100%]" : "translate-x-0")} lg:translate-x-0 transition delay-150 duration-300 ease-linear z-40 max-w-[100%] lg:max-w-[30%] lg:right-0 min-h-screen absolute lg:relative flex-col lg:float-right items-center justify-start sm:min-w-[20%] bg-slate-700/30 h-[50%] pt-4 space-y-6 backdrop-blur-lg lg:flex`}>
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,7 +2,7 @@ import { BiHeart, BiSolidHeart } from "react-icons/bi";
 import { IconContext } from "react-icons";
 import { useState } from 'react';
 
-const Playlist = ({ song }) => {
+const Chart = ({ song }) => {
 	const [songLiked, setSongLiked] = useState(song.liked);
 	const [songActive, setSongActive] = useState(song.active);
 
@@ -18,24 +18,23 @@ const Playlist = ({ song }) => {
 
 	return (
 		<button className={`${songActive ? "bg-slate-400/30" : ""} w-[100%]`} onClick={songHandlerClick}>
-			<div className="w-full flex flex-row lg:justify-center justify-between lg:px-8 px-6 justify-items-center content-between hover:opacity-90 hover:scale-105 transition delay-150 duration-300 ease-linear">
+			<div className="w-full flex flex-row lg:justify-left justify-left lg:px-8 px-6 justify-items-center items-center content-between hover:opacity-90 hover:scale-105 transition delay-150 duration-300 ease-linear">
 				<img
-					className="h-[8vh] w-[8vh] sm:w-[8vh] sm:h-[8vh] object-cover mr-2 float-left rounded-md"
+					className="h-[4vh] w-[4vh] sm:w-[4vh] sm:h-[4vh] object-cover mr-2 float-left rounded-md"
 					src={song.coverURL}
 					alt="coverImg"
 				/>
-				<div className="flex flex-col justify-items-center text-left justify-center pl-2 pr-20">
+				<div className="flex flex-col justify-items-center items-center justify-center pl-2 pr-20">
 					<p className="text-base text-white">{song.label}</p>
 					<p className="text-sm text-slate-400">{song.author}</p>
 				</div>
-				<button className="playButton float-right" onClick={likeHandlerClick}>
-					<IconContext.Provider value={{ size: "1.5rem", color: "#27AE60" }}>
-						{songLiked ? <BiSolidHeart /> : <BiHeart />}
-					</IconContext.Provider>
-				</button>
+                <div className="text-white text-sm flex justify-center justify-items-center items-center">
+                    <p>Прослушиваний: <br/>100000</p>
+                </div>
 			</div>
+            
 		</button>
 	);
 };
 
-export default Playlist;
+export default Chart;
