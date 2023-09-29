96d9e2d7288ec4a018ffa2b9ac3cfb70b73e0d99
Etyhae <84091920+Etyhae@users.noreply.github.com>
2023-09-29 12:31:33 +0300

    chore(App.js) redesign some elements (#1)
    Co-authored-by: ghoul

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import "./App.css";
 import Player from "./components/Player.js";
 import Playlist from "./components/Playlist";
 
+import { IconContext } from "react-icons";
+import { BiMenu } from "react-icons/bi";
+
+import React, { useState} from "react";
+
 const song = {
 	playlistID: 29219219129,
 	playlistName: "DRESSCODE",
@@ -9,7 +14,7 @@ const song = {
 	author: "Nikitata",
 	songURL:
 		"https://s44vlx.storage.yandex.net/get-mp3/b9285fe3ea264bbde7e2bbbdd7076f8e/000602288cf0b268/rmusic/U2FsdGVkX1-AOE5itjTSUhQXPHTi6UVI8voIpe-dWxTdUq9HEiXG0Wmj4k-tCrybtrFOMPHg-dURnsUr4pAhRrsqD5EihtJW8_23Wv6_QXE/67f54a816258fed87d308f7ad72206ba5aa7d8b403e29531f90240a37c589c49/19171?track-id=92228791&play=false",
-	coverURL: "https://i.ytimg.com/vi/wC5OFVMCLS8/maxresdefault.jpg",
+	coverURL: "https://avatars.yandex.net/get-music-content/4382102/c247b5fa.a.18452239-1/800x800",
 	active: false,
 	liked: false
 };
@@ -21,7 +26,7 @@ const song2 = {
 	author: "Nikitata",
 	songURL:
 		"https://s44vlx.storage.yandex.net/get-mp3/b9285fe3ea264bbde7e2bbbdd7076f8e/000602288cf0b268/rmusic/U2FsdGVkX1-AOE5itjTSUhQXPHTi6UVI8voIpe-dWxTdUq9HEiXG0Wmj4k-tCrybtrFOMPHg-dURnsUr4pAhRrsqD5EihtJW8_23Wv6_QXE/67f54a816258fed87d308f7ad72206ba5aa7d8b403e29531f90240a37c589c49/19171?track-id=92228791&play=false",
-	coverURL: "https://i.ytimg.com/vi/wC5OFVMCLS8/maxresdefault.jpg",
+	coverURL: "https://avatars.yandex.net/get-music-content/4382102/c247b5fa.a.18452239-1/800x800",
 	active: false,
 	liked: false
 };
@@ -33,26 +38,30 @@ const song3 = {
 	author: "Nikitata",
 	songURL:
 		"https://s44vlx.storage.yandex.net/get-mp3/b9285fe3ea264bbde7e2bbbdd7076f8e/000602288cf0b268/rmusic/U2FsdGVkX1-AOE5itjTSUhQXPHTi6UVI8voIpe-dWxTdUq9HEiXG0Wmj4k-tCrybtrFOMPHg-dURnsUr4pAhRrsqD5EihtJW8_23Wv6_QXE/67f54a816258fed87d308f7ad72206ba5aa7d8b403e29531f90240a37c589c49/19171?track-id=92228791&play=false",
-	coverURL: "https://i.ytimg.com/vi/wC5OFVMCLS8/maxresdefault.jpg",
+	coverURL: "https://avatars.yandex.net/get-music-content/4382102/c247b5fa.a.18452239-1/800x800",
 	active: true,
 	liked: true
 };
 
 const songs = [song, song2, song3];
 
+
 function App() {
+	const [isHidden, setIsHidden] = useState(true);
+
+	const handleClick = e => {
+	  setIsHidden(isHidden => !isHidden);
+	};
+
 	return (
-		<div>
-			<div
-				className="text-center bg-stone-800 bg-cover bg-center bg-no-repeat"
-				style={{ backgroundImage: `url(${song.coverURL})` }}
-			>
-				<aside className="flex flex-col float-right items-center justify-start min-w-[20%] bg-transparent h-[50%] pt-4 space-y-6 backdrop-blur-lg">
+		<div className="min-h-screen">
+			<div className="text-center bg-stone-800 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${song.coverURL})` }}>
+				<aside className={`${(isHidden? "translate-x-[100%]" : "translate-x-0")} lg:translate-x-0 transition delay-150 duration-300 ease-linear z-40 max-w-[100%] lg:max-w-[30%] lg:right-0 min-h-screen absolute lg:relative flex-col lg:float-right items-center justify-start sm:min-w-[20%] bg-slate-700/30 h-[50%] pt-4 space-y-6 backdrop-blur-lg lg:flex`}>
 					{songs.map((song) => (
 						<Playlist song={song} />
 					))}
 				</aside>
-				<header className="min-h-screen min-w-[75%] flex flex-col items-center justify-center text-base text-white backdrop-blur-lg backdrop-brightness-50 relative">
+				<header className="h-screen flex flex-col items-center justify-center text-base text-white backdrop-blur-lg backdrop-brightness-50 relative">
 					<div className="absolute inset-x-0 top-0 content-start mt-2">
 						<p className="text-sm text-slate-400">Сейчас играет</p>
 						<p className="text-base">{song.playlistName}</p>
@@ -60,18 +69,28 @@ function App() {
 					<div className="flex flex-col items-center justify-center text-center text-white font-mono mt-4">
 						<img
 							src={song.coverURL}
-							className="song-cover h-[50vh] w-[50vh] object-cover pointer-events-none mb-4 rounded-full"
+							className="song-cover h-[40vh] w-[40vh] object-cover pointer-events-none mb-4 rounded-full"
 							alt="logo"
 						/>
-						<p className="text-2xl font-semibold underline decoration-solid underline-offset-8">
+						
+						<p className="text-2xl font-semibold underline decoration-solid underline-offset-8 leading-loose">
 							{song.label}
 						</p>
 						<p className="text-xl text-slate-400">{song.author}</p>
 					</div>
-					<div className="App">
-						<Player url={song.songURL} />
-					</div>
 				</header>
+				<section className="z-50 fixed flex w-full bg-slate-400/20 p-2 bottom-0">
+					<div className="flex justify-start h-full w-full">
+						<Player url={song.songURL} size="6" />
+					</div>
+					<div className="lg:hidden flex items-center justify-end h-full w-full">
+						<button className="hover:opacity-80" onClick={handleClick}>
+							<IconContext.Provider value={{ size: "6vh", color: "#27AE60" }}>
+								<BiMenu />
+							</IconContext.Provider>
+						</button>
+					</div>
+				</section>
 			</div>
 		</div>
 	);
