482a1c029075a08668d1e52831329dc0a760700f
ghoul <dead@xdd.ru>
2023-09-29 18:02:02 +0300

    Revert "chore(Chart.js) style chart block"
    This reverts commit 6611a6ff9faffcdd401908e78da16571f87508d2, reversing
    changes made to 10f404e4daa52d8a3d76618ab1c792d0093eed45.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { IconContext } from "react-icons";
 import { BiMenu } from "react-icons/bi";
 
 import React, { useState} from "react";
-
 import Chart from "./components/Chart";
 import { FastAverageColor } from "fast-average-color";
 
@@ -67,7 +66,6 @@ function App() {
 	};
 
 	return (
-
 		<div>
 			<div className="min-h-screen">
 				<div className="bg-stone-800 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${song.coverURL})` }}>
@@ -115,37 +113,6 @@ function App() {
 								<Chart song={song} place={index} />
 							))}
 					</div>
-				</div>
-		<div className="min-h-screen">
-			<div className="text-center bg-stone-800 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${song.coverURL})` }}>
-				<aside className={`${(isHidden? "translate-x-[100%]" : "translate-x-0")} lg:translate-x-0 transition delay-150 duration-300 ease-linear z-40 max-w-[100%] lg:max-w-[30%] lg:right-0 min-h-screen absolute lg:relative flex-col lg:float-right items-center justify-start sm:min-w-[20%] bg-slate-700/30 h-[50%] pt-4 space-y-6 backdrop-blur-lg lg:flex`}>
-					{songs.map((song) => (
-						<Playlist song={song} />
-					))}
-				</aside>
-				<header className="h-screen flex flex-col items-center justify-center text-base text-white backdrop-blur-lg backdrop-brightness-50 relative">
-					<div className="absolute inset-x-0 top-0 content-start mt-2">
-						<p className="text-sm text-slate-400">Сейчас играет</p>
-						<p className="text-base">{song.playlistName}</p>
-					</div>
-					<div className="flex flex-col items-center justify-center text-center text-white font-mono mt-4">
-						<img
-							src={song.coverURL}
-							className="song-cover h-[40vh] w-[40vh] object-cover pointer-events-none mb-4 rounded-full"
-							alt="logo"
-						/>
-						
-						<p className="text-2xl font-semibold underline decoration-solid underline-offset-8 leading-loose">
-							{song.label}
-						</p>
-						<p className="text-xl text-slate-400">{song.author}</p>
-					</div>
-				</header>
-				<section className="z-50 fixed flex w-full bg-slate-400/20 p-2 bottom-0">
-					<div className="flex justify-start h-full w-full">
-						<Player url={song.songURL} size="6" />
-					</div>
-
 				</div>
 			</div>
 		</div>
